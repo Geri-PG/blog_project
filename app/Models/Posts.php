6c0443cb3f6b2f316fd611ba1953bdb5df104e51
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-06-18 16:07:14 +0200

    creating user and post tables as well as corresponding models

---
+++ app/Models/Posts.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'title',
+        'short_description',
+        'content',
+        'picture',
+        'user_id',
+        'published_at',
+    ];
+}
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Awesome Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@
 
 
                 <li class="nav-item">
-                    <a class="nav-link" href="/shop">Shop</a>
+                    <a class="nav-link" href="/shop">Blogs</a>
                 </li>
             </ul>
         </div>
